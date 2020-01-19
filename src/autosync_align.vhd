897655cdfc68149aa2d9fa7820f2490dd72271a7
Surinder Singh <surinder312@yahoo.com>
2020-01-19 07:06:58 -0500

    modified state machine and register counter value with PS clk

--- src/autosync_align.vhd
+++ src/autosync_align.vhd
@@ -3,163 +3,184 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use IEEE.math_real.all; 
 entity autosync_align is
-  generic(XXXXX_XXXXXToClkRatio : positive:=2); -- XXXXX_XXXXX_strb period / clk_period
+  generic(cci_strbToClkRatio : positive:=2); -- cci_strb period / clk_period
   port( 
     );
-
 end autosync_align;
- 
 
 architecture behavioral of autosync_align is
-
     -- Program Dependable parameters    
-    signal ps_cnt_p  : std_logic_vector(2 downto 0) := '000';
-    signal ps_cnt_n  : std_logic_vector(2 downto 0) := '000';
-    signal trk_cnt_p : std_logic_vector(2 downto 0) := '000';
-    signal trk_cnt_p : std_logic_vector(2 downto 0) := '000';
-    signal numOfPulsesWithInXXXXX_XXXXX : std_logic_vector(2 downto 0);
- 
-    numOfPulsesWithInXXXXX_XXXXX <= std_logic_vector(to_unsigned(XXXXX_XXXXXToClkRatio, numOfPulsesWithInXXXXX_XXXXX'length)) ;
+    signal numOfPulsesWithIncci_strb : std_logic_vector(2 downto 0);
+
+    signal ps_cnt_p_D  : std_logic_vector(2 downto 0) := '000';
+    signal ps_cnt_n_D  : std_logic_vector(2 downto 0) := '000';
+    signal trk_cnt_p_D : std_logic_vector(2 downto 0) := '000';
+    signal trk_cnt_n_D : std_logic_vector(2 downto 0) := '000';
+    signal ps_cnt_p_Q  : std_logic_vector(2 downto 0) := '000';
+    signal ps_cnt_n_Q  : std_logic_vector(2 downto 0) := '000';
+    signal trk_cnt_p_Q : std_logic_vector(2 downto 0) := '000';
+    signal trk_cnt_n_Q : std_logic_vector(2 downto 0) := '000';
+
+    signal cci_strb    : std_logic :='0';
+    signal ps_incdec   : std_logic :='1';
+
+    numOfPulsesWithIncci_strb <= std_logic_vector(to_unsigned(cci_strbToClkRatio, numOfPulsesWithIncci_strb'length)) ;
     type state_type is ( IDEAL , 
-                         DET_FIRST_EDGE_P, 
-                         DET_FIRST_EDGE_N, 
-                         DET_PS_CNT_VALID_P,
-                         DET_PS_CNT_VALID_N,
-                         DET_TRK_CNT_VALID,
-                         PHS_SHIFT,
-                         XXXXX_XXXXX_LOCKED                                                                      
+                         DETERMINE_FIRST_CLK_EDGE_WITHIN_CCI, 
+                         DETERMINE_NEG_CLK_COUNT_VALID_WITHIN_CCI,
+                         DETERMINE_POS_CLK_COUNT_VALID_WITHIN_CCI,
+                         CHECK_INPUT_CLK_ALIGNMENT_WITH_CCI,
+                         SHIFT_INPUT_CLK_TO_PERFECT_ALIGNMENT,
+                         INTERMITTENT_PHS_SHIFT,
+                         FINAL_PHS_SHIFT,
+                         INPUT_CLK_PHASE_LOCKED_WITH_CCI                                                                      
                          )
     signal state, next_state : state_type ;
-
 begin
-
-  SYNC_PROC : process (PS_CLK)
+  SYNC_PROC_P : process (PS_CLK)
   begin
     if rising_edge(PS_CLK) then
-      if (reset = '1') then
+      if (reset_mmcm = '1' or locked = '0') then
         state <= IDEAL;
-      elsif(XXXXX_XXXXX_strb = '0') -- only change state after XXXXX_XXXXX strb 
+        ps_cnt_p_Q  <= '000'  ;  
+        trk_cnt_p_Q <= '000'  ;   
+      elsif(cci_strb = '1') 
+        ps_cnt_p_Q  <= ps_cnt_p_D  ;  
+        trk_cnt_p_Q <= trk_cnt_p_D ; 
         state <= next_state;
       end if;
     end if;
   end process;
-
-  process_ps_rise : process(cci_strb, PS_CLK) 
+  SYNC_PROC_N : process (PS_CLK)
   begin
-    if(rising_edge(cci_strb) and rising_edge(PS_CLK)) then 
+    if falling_edge(PS_CLK) then
+      if (reset_mmcm = '1' or locked = '0') then
+        ps_cnt_n_Q  <= '000'  ;  
+        trk_cnt_n_Q <= '000'  ; 
+      elsif(cci_strb='1') 
+        ps_cnt_n_Q  <= ps_cnt_n_D  ;  
+        trk_cnt_n_Q <= trk_cnt_n_D ; 
+      end if;
+    end if;
   end process;
-  
 
- 
-  PS_CNT_WITHIN_XXXXX_XXXXX_P : process (PS_CLK)
+  COUNT_PS_CLK_RISING_EDGE_WITHIN_CCI : process (PS_CLK)
   begin 
     if rising_edge(PS_CLK ) then
-
-      if(XXXXX_XXXXX_strb = '0' or reset_mmcm = '1' or locked '0') then 
-        ps_cnt_p <= '000' ;
-      elsif (XXXXX_XXXXX = '1') then 
-        ps_cnt_p <= ps_cnt_p + 1 ;
+      if(cci_strb = '0' or reset_mmcm = '1' or locked = '0') then 
+        ps_cnt_p_D <= '000' ;
+      elsif (cci_strb = '1') then 
+        ps_cnt_p_D <= ps_cnt_p_D + 1 ;
       end if ;
     end if; 
   end process ;
         
-  PS_CNT_WITHIN_XXXXX_XXXXX_N : process (PS_CLK)
+  COUNT_PS_CLK_FALLING_EDGE_WITHIN_CCI : process (PS_CLK)
   begin 
     if falling_edge(PS_CLK) then
-
-      if(XXXXX_XXXXX_strb = '0' or reset_mmcm = '1' or locked '0') then 
-        ps_cnt_n <= '000' ;
-      elsif (XXXXX_XXXXX = '1') then 
-        ps_cnt_n <= ps_cnt_n + 1 ;
+      if(cci_strb = '0' or reset_mmcm = '1' or locked = '0') then 
+        ps_cnt_n_D <= '000' ;
+      elsif (cci_strb = '1') then 
+        ps_cnt_n_D <= ps_cnt_n_D + 1 ;
       end if ;
     end if ;
   end process ;
  
-  PS_CNT_WITHIN_XXXXX_XXXXX_P : process (XXXXX_XXXXX_TRACKIN_CLK)
+  COUNT_INPUT_CLK_RISING_EDGE_WITHIN_CCI : process (INPUT_CLK)
   begin 
-    if rising_edge(XXXXX_XXXXX_TRACKIN_CLK ) then
-
-      if(XXXXX_XXXXX_strb = '0' or reset_mmcm = '1' or locked '0') then 
-        trk_cnt_p <= '000' ;
-      elsif (XXXXX_XXXXX = '1') then 
-        trk_cnt_p <= trk_cnt_p + 1 ;
+    if rising_edge(INPUT_CLK) then
+      if(cci_strb = '0' or reset_mmcm = '1' or locked = '0') then 
+        trk_cnt_p_D <= '000' ;
+      elsif (cci_strb = '1') then 
+        trk_cnt_p_D <= trk_cnt_p_D + 1 ;
       end if ;
     end if;
   end process ;
  
-  PS_CNT_WITHIN_XXXXX_XXXXX_P : process (XXXXX_XXXXX_TRACKIN_CLK)
+  COUNT_INPUT_CLK_FALLING_EDGE_WITHIN_CCI : process (INPUT_CLK)
   begin 
-    if falling_edge(XXXXX_XXXXX_TRACKIN_CLK ) then
-
-      if(XXXXX_XXXXX_strb = '0' or reset_mmcm = '1' or locked '0') then 
-        trk_cnt_n <= '000' ;
-      elsif (XXXXX_XXXXX = '1') then 
-        trk_cnt_n <= trk_cnt_n + 1 ;
+    if falling_edge(INPUT_CLK) then
+      if(cci_strb = '0' or reset_mmcm = '1' or locked = '0') then 
+        trk_cnt_n_D <= '000' ;
+      elsif (cci_strb = '1') then 
+        trk_cnt_n_D <= trk_cnt_n_D + 1 ;
       end if ;
     end if;
   end process ;
- 
   STATE_MACHINE_PROC : process (
-                                ps_cnt_p, ps_cnt_n,
-                                trk_cnt_p, trk_cnt_n, 
-                                PS_CLK, XXXXX_XXXXX_strb, ps_done
-
+                                ps_cnt_p_Q, ps_cnt_n_Q,
+                                trk_cnt_p_Q, trk_cnt_n_Q,
+                                ps_done
                               )
-  begin 
+  begin
     if(reset_mmcm = '1' or locked = '0') then 
       next_state <= IDEAL ;
     else
-
       case(state) is 
         when IDEAL => 
-          if(rising_edge(XXXXX_XXXXX_strb)) then 
+          if(cci_strb = '1') then 
             next_state <= DET_FIRST_EDGE ;
+            ps_incdec  <= '1';
           end if;
-        when DET_FIRST_EDGE_P =>
--- protection to avoid combinotorial circuit to re-evalute next state within XXXXX_XXXXX strb
-
+        when DETERMINE_FIRST_CLK_EDGE_WITHIN_CCI =>
+          -- protection to avoid combinotorial circuit to 
+          -- re-evalute next state within cci_strb interval
           if(next_state /= state)
             next_state <= next_state ; 
-          elsif(XXXXX_XXXXX_strb = '1' and ps_cnt_p = '001') then -- first rising edge within XXXXX_XXXXX
-
-            next_state <= DET_PS_CNT_VALID_P ;
-          elsif(XXXXX_XXXXX_strb = '1' and ps_cnt_n = '001') then -- first falling edge within XXXXX_XXXXX
-
-            next_state <= DET_PS_CNT_VALID_N ;
+          elsif(ps_cnt_p_Q = '001' and ps_cnt_n_Q = '000') then -- first rising edge within cci_strb
+            next_state <= DETERMINE_POS_CLK_COUNT_VALID_WITHIN_CCI ;
+          elsif(ps_cnt_n_Q = '001' and ps_cnt_p_Q = '000') then -- first falling edge within cci_strb
+            next_state <= DETERMINE_NEG_CLK_COUNT_VALID_WITHIN_CCI ;
           end if;
-        when DET_PS_CNT_VALID_P =>
+        when DETERMINE_NEG_CLK_COUNT_VALID_WITHIN_CCI =>
           if(next_state /= state)
             next_state <= next_state ;
-          elsif(XXXXX_XXXXX_strb = '1' and ps_cnt_p = numOfPulsesWithInXXXXX_XXXXX) then 
-            next_state <= DET_TRK_CNT_VALID_P;
+          elsif(ps_cnt_n_Q = numOfPulsesWithIncci_strb) then
+            next_state <= CHECK_INPUT_CLK_ALIGNMENT_WITH_CCI;
           end if;
-        when DET_PS_CNT_VALID_N =>
+        when DETERMINE_POS_CLK_COUNT_VALID_WITHIN_CCI =>
           if(next_state /= state)
             next_state <= next_state ;
-          elsif(XXXXX_XXXXX_strb = '1' and ps_cnt_n = numOfPulsesWithInXXXXX_XXXXX) then 
-            next_state <= DET_TRK_CNT_VALID_N;
-            XXXXX_XXXXX_counter_start <= '1';
+          elsif(ps_cnt_p_D = numOfPulsesWithIncci_strb) then 
+            next_state <= CHECK_INPUT_CLK_ALIGNMENT_WITH_CCI;
+            cci_strb_counter_start <= '1';
           end if;
-        when DET_TRK_CNT_VALID =>
+        when CHECK_INPUT_CLK_ALIGNMENT_WITH_CCI =>
           if(next_state /= state)
             next_state <= next_state ;
-          elseif( trk_cnt_p = '001' and ps_cnt_p = '000' ) then 
-            next_state <= XXXXX_XXXXX_LOCKED ;
-          elsif(falling_edge(XXXXX_XXXXX_strb) and ps_cnt_p = trk_cnt_p) then 
-            next_state <= PHS_SHIFT;
+          -- tracking clk rising edge comes first before reference clock. This condition exist when tracking 
+          -- clk moved one tap delay after perfect alignment with cci strb
+          elsif( trk_cnt_p_Q = '001' and ps_cnt_p_Q = '000' ) then 
+            next_state <= SHIFT_INPUT_CLK_TO_PERFECT_ALIGNMENT ;
+          elsif(cci_strb = '0') then 
+            next_state <= INTERMITTENT_PHS_SHIFT;
             ps_signal_q <= '1'
           end if;
-        when PHS_SHIFT =>
+        when SHIFT_INPUT_CLK_TO_PERFECT_ALIGNMENT =>
+          if(next_state /= state)
+            next_state <= next_state ;
+          elsif(cci_strb = '0') then
+            ps_incdec  <= '0' ; 
+            next_state <= FINAL_PHS_SHIFT;
+            ps_signal_q <= '1'
+            next_state <= IDEAL;
+          end if;
+        when INTERMITTENT_PHS_SHIFT =>
           if(next_state /= state)
             next_state <= next_state ;
           elsif(ps_done = '1') then
             next_state <= IDEAL;
           end if;
- 
-        when XXXXX_XXXXX_LOCKED =>
+        when FINAL_PHS_SHIFT =>
           if(next_state /= state)
             next_state <= next_state ;
-          if(XXXXX_XXXXX_counter_expired) then 
+          elsif(ps_done = '1') then
+            next_state <= INPUT_CLK_PHASE_LOCKED_WITH_CCI;
+          end if;
+        when INPUT_CLK_PHASE_LOCKED_WITH_CCI =>
+          if(next_state /= state)
+            next_state <= next_state ;
+          if(cci_strb_counter_expired) then 
             next_state <= IDEAL;
           enf if;
 end architecture;
